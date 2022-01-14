e88bdd9fa5e0d516199fbbcea9e73edd55ae1a05
Dhans <sgdhans@gmail.com>
2022-01-14 12:19:17 +0530

    initial commit

---
+++ spotify-clone/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h2>starting now</h2>
+      <main>
+
+      </main>
+    </div>
+  )
+}
